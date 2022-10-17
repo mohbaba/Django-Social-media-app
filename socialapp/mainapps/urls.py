8dfb7c18df5d33b778373c5d5bff30a975e26699
mohbaba <babamu09@gmail.com>
2022-10-17 09:38:07 +0100

    Account settings updated

--- socialapp/mainapps/urls.py
+++ socialapp/mainapps/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import index,signup,signin
+from .views import index,signup,signin,logout,settings
 
 urlpatterns = [
     path('', index ,name='index'),
     path('signup',signup, name = 'signup'),
-    path('signin',signin, name = 'signin')
-    
+    path('signin',signin, name = 'signin'),
+    path('logout',logout, name = 'logout'),
+    path('settings',settings, name = 'settings')
 ]
--- socialapp/mainapps/views.py
+++ socialapp/mainapps/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User,  auth
 from django.contrib import messages
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from .models import Profile
 # Create your views here.
-
+@login_required(login_url='signin')
 def index(request):
     return render(request,'index.html')
 
@@ -27,12 +28,16 @@ def signup(request):
                 user.save()
                 
                 #log user in and direct to settings page
+                
+                user_login = auth.authenticate(username=username, password=password)
+                auth.login(request,user_login)
+                
                 #Create profile for user
                 
                 user_model = User.objects.get(username=username)
                 new_profile = Profile.objects.create(user=user_model,id_user = user_model.id)
                 new_profile.save()
-                return redirect('signup')
+                return redirect('settings')
         else:
             messages.info(request, 'Passwords dont match')
             return redirect('signup')
@@ -55,3 +60,11 @@ def signin(request):
             return redirect('signin')
     else:
         return render(request,'signin.html')
+@login_required(login_url='signin')    
+def logout(request):
+    auth.logout(request)
+    return redirect('signin')
+
+@login_required(login_url='signin')
+def settings(request):
+    return render(request,'setting.html')
