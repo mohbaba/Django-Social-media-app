1a043a7c26e60f41e5827644795eaecbde557258
mohbaba <babamu09@gmail.com>
2022-11-16 02:58:54 +0100

    Post feed updated

--- socialapp/mainapps/views.py
+++ socialapp/mainapps/views.py
@@ -4,6 +4,7 @@ from django.contrib import messages
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from .models import Profile , Post, LikePost, FollowersCount
+from itertools import chain
 
 # Create your views here.
 
@@ -11,8 +12,24 @@ from .models import Profile , Post, LikePost, FollowersCount
 def index(request):
     user_object = User.objects.get(username=request.user.username)
     user_profile = Profile.objects.get(user=user_object)
+    
+    user_following_list= []
+    feed = []
+    
+    user_following = FollowersCount.objects.filter(follower = reuest.user.username)
+    for users in user_following:
+        user_following_list.append(users.user)
+    
+    for usernames in user_following_list:
+        feed_list = Post.objects.filter(user = usernames)
+        feed.append(feed_list)
+        
+    feed_list = list(chain(*feed))
+    
     posts = Post.objects.all()
-    return render(request,'index.html', {'user_profile' : user_profile, 'posts':posts} )
+    return render(request,'index.html', {'user_profile' : user_profile, 'posts':feed_list} )
+
+
 
 @login_required(login_url='signin')
 def profile(request,pk):
